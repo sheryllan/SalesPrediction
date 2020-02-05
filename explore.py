70b4e7b0a43cdcd3c7a0b13788c9ad4fd81a1408
slan <slan@liquidcapital.com>
2020-02-05 19:01:55 +0000

    added the data file

--- explore.py
+++ explore.py
@@ -1,9 +1,8 @@
-import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import pandas as pd
 from enum import Enum
-
+import os
 
 
 class Fields(str, Enum):
@@ -12,7 +11,7 @@ class Fields(str, Enum):
     sale_value = 'sale_value'
 
 
-fname = '/home/slan/features/t0_predict_data.csv'
+fname = 't0_predict_data.csv'
 data = pd.read_csv(fname, parse_dates=[Fields.trade_time])
 
 channels = data.groupby(Fields.channel)
@@ -25,7 +24,8 @@ for ax, (channel, df_channel) in zip(axes, channels):
     x_data = df_channel[Fields.trade_time].map(mdates.date2num)
     ax.plot(x_data, df_channel[Fields.sale_value])
 
-fig.savefig('/home/slan/features/SalesByTime.pdf')
+os.makedirs('results', exist_ok=True)
+fig.savefig('results/SalesByTime.pdf')
 
 
 
